e8f7ccc75237b810335c271859d53ccf80ac09a0
matthiasjrichter <draussensaiter@gmail.com>
2023-04-17 14:15:54 +0200

    added time and date

--- assets/lcars.js
+++ assets/lcars.js
@@ -17,3 +17,18 @@ $(document).ready(function() {
     });
 
 });
+
+/* Additions by Matthias J. Richter */
+
+// var datetime = new Date();
+// console.log(datetime);
+// document.getElementById("time").textContent = datetime; //it will print on html page
+
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", "  ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
